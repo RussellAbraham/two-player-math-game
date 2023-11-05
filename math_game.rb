7f5152ce8ffc4a826163dfbee244ad5a0ff96079
RussellAbraham <russell_a@live.com>
2023-11-04 17:42:41 -0700

    Setup logic for MathGame class

--- math_game.rb
+++ math_game.rb
@@ -9,8 +9,33 @@ class MathGame
     @current_player = @players.first
   end
 
+  # Method to start the game loop
   def start_turn
-  
+    question = generate_question
+    puts "#{current_player.name}, what is #{question.text}?"
+    answer = gets.chomp.to_i
+    correct = question.correct?(answer)
+    current_player.answer_question(correct)
   end
 
+  # Method to generate a new question
+  def generate_question
+    Question.new(rand(1..20), rand(1..20))
+  end
+
+  # Method to switch the current player
+  def switch_player
+    @current_player = (current_player == @players.first) ? @players.last : @players.first
+  end
+
+  # Method to announce the winner at the end of the game
+  def announce_winner
+    winner = @players.max_by(&:score)
+    puts "#{winner.name} wins with a score of #{winner.score}!"
+  end
+
+  def current_player
+    @current_player
+  end
+  
 end
--- question.rb
+++ question.rb
@@ -3,7 +3,7 @@ puts 'Question loaded...'
 class Question
 
   # Getter method for initilized numbers
-  attr_reader :num1, num2
+  attr_reader :num1, :num2
 
   # Properties for the random numbers to exist
   def initialize(num1, num2)
