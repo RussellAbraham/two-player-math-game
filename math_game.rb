794c1288523ed2891905a166370f8fb0b509614d
RussellAbraham <russell_a@live.com>
2023-11-04 17:22:24 -0700

    Add methods to player class matching desired behavior

--- math_game.rb
+++ math_game.rb
@@ -3,7 +3,7 @@ puts 'MathGame loaded...'
 class MathGame
 
   def initialize(player1, player2)
-    @player = [player1, player2]
+    @players = [player1, player2]
   end
 
 end
--- player.rb
+++ player.rb
@@ -2,8 +2,29 @@ puts 'Player loaded...'
 
 class Player
 
+  # Method to display the player's current score, name, and lives
+  attr_reader :name, :score, :lives
+
   def initialize(name)
     @name = name
+    @score = 0
+    @live = 3
+  end
+
+  # Method to answer a question, decrementing lives if the answer is incorrect
+  def answer_question(correct)
+    if correct
+      @score += 1
+      puts "Correct! #{@name}'s score : #{@score}"
+    else
+      @lives -= 1
+      puts "Incorrect! #{@name} loses a life. Lives remaining: #{@lives}"
+    end
+  end
+
+  # Method to check if the player has lost all lives
+  def hase_lost?
+    @lives <= 0
   end
 
 end
